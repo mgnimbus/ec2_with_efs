ac5c4131c7f90e652856cdc4cfb48ba7df142897
gowtham <medagowtham2@yahoo.com>
2023-06-05 13:20:37 +0530

    modified json parse

--- scripts/json/parse_json_2.py
+++ scripts/json/parse_json_2.py
@@ -4,7 +4,9 @@ import gzip
 
 
 def transformLogEvent(log_event):
-    if isinstance(log_event, str):
+    if isinstance(log_event, dict) and 'message' in log_event:
+        return log_event
+    elif isinstance(log_event, str):
         return {'message': log_event.rstrip('\n')}
     else:
         return log_event
@@ -12,26 +14,25 @@ def transformLogEvent(log_event):
 
 def lambda_handler(event, context):
     processed_records = []
-
+    
     for record in event['records']:
         data = json.loads(gzip.decompress(base64.b64decode(record['data'])))
         recId = record['recordId']
         result = {
             'recordId': recId
         }
-
+        
         if data['messageType'] == 'CONTROL_MESSAGE':
             result['result'] = 'Dropped'
         elif data['messageType'] == 'DATA_MESSAGE':
-            joinedJsonStr = ''.join(transformLogEvent(
-                e)['message'] for e in data['logEvents'] if isinstance(e, str))
+            joinedJsonStr = ''.join(transformLogEvent(e)['message'] for e in data['logEvents'] if 'message' in transformLogEvent(e))
             dataBytes = joinedJsonStr.encode("utf-8")
             encodedData = base64.b64encode(dataBytes).decode('utf-8')
             result['data'] = encodedData
             result['result'] = 'Ok'
         else:
             result['result'] = 'ProcessingFailed'
-
+        
         processed_records.append(result)
 
     print('Successfully processed {} records.'.format(len(event['records'])))
@@ -40,7 +41,7 @@ def lambda_handler(event, context):
         'records': processed_records,
         'result': 'Success'  # Add the 'result' field to the response
     }
-
+    
     result = {
         'result': 'Success',  # Update the overall result based on the processing
         'records': processed_records
